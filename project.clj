4555ec5a8ab97307043e5f75c7f61081b038b7a4
Michael Klishin <michael@clojurewerkz.org>
2014-11-15 19:35:26 +0300

    Depend on Clojure 1.6

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject clojurewerkz/vclock "1.1.0-SNAPSHOT"
   :description "A vector clock implementation roughly ported from Riak Core"
-  :dependencies [[org.clojure/clojure  "1.3.0"]
+  :dependencies [[org.clojure/clojure  "1.6.0"]
                  [clojurewerkz/support "0.7.0-beta1"]]
   :profiles {:1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
              :1.5 {:dependencies [[org.clojure/clojure "1.5.0"]]}
